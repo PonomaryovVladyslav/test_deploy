e2e4054a1b7b862f80795e2f0f3f0e5b0fe3697c
PonomaryovVladyslav <Blackbird1Blackbird1!>
2020-12-15 20:55:37 +0200

    comment db settings

--- Model4/settings_prod.py
+++ Model4/settings_prod.py
@@ -2,14 +2,14 @@ import os
 
 DEBUG = False
 ALLOWED_HOSTS = ['100.26.171.115']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME'),
-        'USER': os.environ.get('DBUSER'),
-        'PASSWORD': os.environ.get('DBPASS'),
-        'HOST': os.environ.get('DBHOST', '127.0.0.1'),
-        'PORT': os.environ.get('DBPORT', '5432'),
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DBNAME'),
+#         'USER': os.environ.get('DBUSER'),
+#         'PASSWORD': os.environ.get('DBPASS'),
+#         'HOST': os.environ.get('DBHOST', '127.0.0.1'),
+#         'PORT': os.environ.get('DBPORT', '5432'),
+#     }
+# }
